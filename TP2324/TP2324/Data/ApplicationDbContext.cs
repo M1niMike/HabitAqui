64408a9dc6a4b8a8a3674cd48256052177af4265
Pedro Henriques <33936309+pedromnh@users.noreply.github.com>
2023-10-11 14:33:21 +0100

    Testings

--- TP2324/TP2324/Data/ApplicationDbContext.cs
+++ TP2324/TP2324/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Client> Clients { get; set; }
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Manager> Managers { get; set; }
+    public DbSet<Testing> Testings { get; set; }
 }
 
